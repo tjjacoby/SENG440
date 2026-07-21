f649a7bc2f82aa607a07d586220425233e51f4d4
mdhesi <mdhesi334@gmail.com>
2026-07-21 15:25:07 -0700

    O2 opt assembly

--- fixedpoint.s
+++ fixedpoint.s
@@ -7,153 +7,84 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"butterworth.c"
 	.text
-	.section	.rodata
-	.align	2
-	.type	b0, %object
-	.size	b0, 4
-b0:
-	.word	1105
-	.align	2
-	.type	b1, %object
-	.size	b1, 4
-b1:
-	.word	2210
-	.align	2
-	.type	b2, %object
-	.size	b2, 4
-b2:
-	.word	1105
-	.align	2
-	.type	a1, %object
-	.size	a1, 4
-a1:
-	.word	18727
-	.align	2
-	.type	a2, %object
-	.size	a2, 4
-a2:
-	.word	-6763
+	.section	.rodata.str1.4,"aMS",%progbits,1
 	.align	2
 .LC0:
 	.ascii	"y_fixed[%2d] = %+9.5f\012\000"
-	.text
+	.section	.text.startup,"ax",%progbits
 	.align	1
+	.p2align 2,,3
 	.global	main
 	.syntax unified
 	.thumb
 	.thumb_func
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 152
-	@ frame_needed = 1, uses_anonymous_args = 0
-	push	{r7, lr}
-	sub	sp, sp, #152
-	add	r7, sp, #0
-	mov	r3, #49152
-	movt	r3, 65535
-	str	r3, [r7, #148]
-	mov	r3, #49152
-	movt	r3, 65535
-	str	r3, [r7, #144]
-	mov	r3, #49152
-	movt	r3, 65535
-	str	r3, [r7, #140]
-	mov	r3, #49152
-	movt	r3, 65535
-	str	r3, [r7, #136]
-	movs	r3, #0
-	str	r3, [r7, #132]
-	b	.L2
-.L3:
-	ldr	r3, [r7, #132]
-	lsls	r3, r3, #2
-	adds	r3, r3, #152
-	add	r3, r3, r7
+	@ args = 0, pretend = 0, frame = 120
+	@ frame_needed = 0, uses_anonymous_args = 0
+	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
 	mov	r2, #16384
-	str	r2, [r3, #-148]
-	ldr	r3, [r7, #132]
-	adds	r3, r3, #1
-	str	r3, [r7, #132]
+	vpush.64	{d8}
+	sub	sp, sp, #124
+	sub	r8, sp, #4
+	add	r1, sp, #116
+	mov	r3, r8
 .L2:
-	ldr	r3, [r7, #132]
-	cmp	r3, #29
-	ble	.L3
-	movs	r3, #0
-	str	r3, [r7, #128]
-	b	.L4
-.L5:
-	ldr	r3, [r7, #128]
-	lsls	r3, r3, #2
-	adds	r3, r3, #152
-	add	r3, r3, r7
-	ldr	r3, [r3, #-148]
-	movw	r2, #1105
-	mul	r2, r3, r2
-	movw	r1, #2210
-	ldr	r3, [r7, #148]
-	mul	r3, r1, r3
-	add	r2, r2, r3
-	movw	r1, #1105
-	ldr	r3, [r7, #144]
-	mul	r3, r1, r3
-	add	r2, r2, r3
-	movw	r1, #18727
-	ldr	r3, [r7, #140]
-	mul	r3, r1, r3
-	add	r2, r2, r3
-	movw	r3, #58773
+	str	r2, [r3, #4]!
+	cmp	r1, r3
+	bne	.L2
+	ldr	r10, .L8+8
+	mov	r3, #49152
 	movt	r3, 65535
-	ldr	r1, [r7, #136]
-	mul	r3, r1, r3
-	add	r3, r3, r2
-	asrs	r3, r3, #14
-	str	r3, [r7, #124]
-	ldr	r3, [r7, #124]
-	vmov	s15, r3	@ int
-	vcvt.f64.s32	d7, s15
-	vldr.64	d6, .L7
-	vdiv.f64	d5, d7, d6
-	vmov	r2, r3, d5
-	ldr	r1, [r7, #128]
-	ldr	r0, .L7+8
+	movw	r9, #58773
+	movt	r9, 65535
+	mov	fp, #0
+	mov	r6, r3
+	mov	r2, r3
+	vldr.64	d8, .L8
 .LPIC0:
-	add	r0, pc
+	add	r10, pc
+	mov	r5, r3
+.L3:
+	ldr	r7, [r8, #4]!
+	movw	r4, #2210
+	movw	lr, #1105
+	movw	ip, #18727
+	mul	r4, r5, r4
+	mov	r1, fp
+	mov	r0, r10
+	add	fp, fp, #1
+	mla	r4, lr, r7, r4
+	mla	r4, lr, r2, r4
+	mla	r4, ip, r6, r4
+	mla	r4, r9, r3, r4
+	asrs	r4, r4, #14
+	vmov	s15, r4	@ int
+	vcvt.f64.s32	d7, s15
+	vmul.f64	d7, d7, d8
+	vmov	r2, r3, d7
 	bl	printf(PLT)
-	ldr	r3, [r7, #148]
-	str	r3, [r7, #144]
-	ldr	r3, [r7, #128]
-	lsls	r3, r3, #2
-	adds	r3, r3, #152
-	add	r3, r3, r7
-	ldr	r3, [r3, #-148]
-	str	r3, [r7, #148]
-	ldr	r3, [r7, #140]
-	str	r3, [r7, #136]
-	ldr	r3, [r7, #124]
-	str	r3, [r7, #140]
-	ldr	r3, [r7, #128]
-	adds	r3, r3, #1
-	str	r3, [r7, #128]
-.L4:
-	ldr	r3, [r7, #128]
-	cmp	r3, #29
-	ble	.L5
-	movs	r3, #0
-	mov	r0, r3
-	adds	r7, r7, #152
-	mov	sp, r7
+	mov	r2, r5
+	mov	r3, r6
+	mov	r5, r7
+	mov	r6, r4
+	cmp	fp, #30
+	bne	.L3
+	movs	r0, #0
+	add	sp, sp, #124
 	@ sp needed
-	pop	{r7, pc}
-.L8:
+	vldm	sp!, {d8}
+	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
+.L9:
 	.align	3
-.L7:
+.L8:
 	.word	0
-	.word	1087373312
+	.word	1058013184
 	.word	.LC0-(.LPIC0+4)
 	.size	main, .-main
 	.ident	"GCC: (Debian 14.2.0-19) 14.2.0"
